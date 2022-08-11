7064410fd3b4113fe90d898e7ec5cdceb5092e53
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2022-08-11 22:11:40 +0200

    Update deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,3 +1,14 @@
+
+echo '>>>>>>>>>>> checkpoint 1'
+
 aws s3 cp --recursive  ./www s3://random1994/
+
+echo '>>>>>>>>>>> checkpoint 2'
+
 aws s3 cp  --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://random1994/
+
+echo '>>>>>>>>>>> checkpoint 3'
+
 # aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://random1994/
+
+echo '>>>>>>>>>>> checkpoint 4'
