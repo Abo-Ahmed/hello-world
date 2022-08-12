35029f10d1bc548906cbd15d6ec86a8ad85a3d31
Mahmoud Mohammed Taha <mahmoudaboahmed6@gmail.com>
2022-08-12 02:37:57 +0200

    v0

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -2,8 +2,10 @@ import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
 export const sequelize = new Sequelize(
-  'postgres://postgres:postgres@database-1.cfk009rrsstw.us-east-1.rds.amazonaws.com:5432/postgres'
-);
+  // 'postgres://postgres:postgres@database-1.cfk009rrsstw.us-east-1.rds.amazonaws.com:5432/postgres'
+  "postgres://" + config.username + ":" + config.password + "@" + config.host + ":" + config.dbPort + "/" + config.database 
+
+  );
 
 // export const sequelize = new Sequelize({
 //   username: config.username,
